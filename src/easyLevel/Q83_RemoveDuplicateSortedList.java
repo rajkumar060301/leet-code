8ca6f0a66110a8a7b1f2c31ffa2df35311b21cd9
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2025-01-08 23:56:14 +0530

    add multiply string solution of leetcode

--- src/easyLevel/Q83_RemoveDuplicateSortedList.java
+++ src/easyLevel/Q83_RemoveDuplicateSortedList.java
@@ -4,18 +4,6 @@ import java.util.LinkedList;
 
 class Q83_RemoveDuplicateSortedList {
 
-    public static ListNode deleteDuplicates(ListNode head) {
-
-        ListNode curr = head;
-
-        while (curr != null) {
-            while (curr.next != null && curr.val == curr.next.val)
-                curr.next = curr.next.next;
-            curr = curr.next;
-        }
-
-        return head;
-    }
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
