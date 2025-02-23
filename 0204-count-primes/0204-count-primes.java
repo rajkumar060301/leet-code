277c5979055b444c9d850d47cf8f167804f627c6
Raj Kumar <rajkumarktr10@gmail.com>
2025-02-23 21:37:34 +0530

    Time: 99 ms (64.94%), Space: 49.1 MB (71.51%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int countPrimes(int n) {
+        // If n is less than or equal to 2, there are no primes
+        if (n <= 2) return 0;
+
+        // Create a boolean array to mark whether numbers are prime
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                // Mark all multiples of i as non-prime
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Count the number of primes
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
