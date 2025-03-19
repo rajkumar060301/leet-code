454cb5c1ecfa77a14d5fd789aeceffb839088cc1
Raj Kumar <rajkumarktr10@gmail.com>
2025-03-19 09:30:36 +0530

    Time: 1 ms (29.83%), Space: 40.9 MB (11.23%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int maximumSwap(int num) {
+        String numStr = Integer.toString(num); 
+        int n = numStr.length();
+        int maxNum = num; 
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                char[] numeral = numStr.toCharArray(); 
+
+                char temp = numeral[i];
+                numeral[i] = numeral[j];
+                numeral[j] = temp;
+
+                int tempNum = Integer.parseInt(new String(numeral)); 
+
+                maxNum = Math.max(maxNum, tempNum);
+            }
+        }
+
+        return maxNum;
+    }
+}
