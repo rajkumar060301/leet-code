054ab0a001281b70011e63c07096a51798c19e4a
Raj Kumar <rajkumarktr10@gmail.com>
2025-07-06 23:36:02 +0530

    Time: 4 ms (100%), Space: 44.3 MB (26.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x){
+        if(0 > x) return false;
+        int original = x;
+        int add = 0;
+
+        while(x > 0){
+            int rem = x % 10;
+            add = (add * 10) + rem;
+            x = x / 10;
+        }
+        if(add == original) {
+            return true;
+        } 
+        return false;
+
+    }
+}
