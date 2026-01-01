f62c802db94089090ddaafab631764e48fdbd448
Raj Kumar <rajkumarktr10@gmail.com>
2026-01-02 00:08:31 +0530

    Time: 6 ms (42.9%), Space: 46.7 MB (8.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,13 +2,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         String str1 = s.toLowerCase();
         String str2 = t.toLowerCase();
-        char[] charArr1 = str1.toCharArray();
-        char[] charArr2 = str2.toCharArray();
-        Arrays.sort(charArr1);
-        Arrays.sort(charArr2);
 
-        if(s.length() == t.length() &&  Arrays.equals(charArr1, charArr2)){
-            return true;
+        if(s.length() == t.length()){
+            char[] charArr1 = str1.toCharArray();
+            char[] charArr2 = str2.toCharArray();
+            Arrays.sort(charArr1);
+            Arrays.sort(charArr2);
+
+            if(Arrays.equals(charArr1, charArr2)){
+                return true;
+            }
+
         }
         return false;
     }
