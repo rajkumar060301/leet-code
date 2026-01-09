f592968cce98a437b95d4c60a28bf495e544dbed
Raj Kumar <rajkumarktr10@gmail.com>
2026-01-09 21:21:50 +0530

    Time: 0 ms (100%), Space: 44.3 MB (36.09%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,25 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
     public int findPeakElement(int[] nums) {
-        // Find the maximum value
-        int maxValue = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (num > maxValue) {
-                maxValue = num;
-            }
+        int start = 1;
+        int end = nums.length;
+        int n =nums.length;
+        if(nums.length ==1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            mid = (int) Math.floor(mid);
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
+            else if(nums[mid] < nums[mid+1]) start = mid + 1;
         }
 
-        // Print the sorted array (optional)
-        int[] copy = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(copy);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == maxValue) {
-                return i;
-            }
-        }
-
-        return -1; // Should never reach here
+        return -1;
     }
 }
