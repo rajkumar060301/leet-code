faef579eda11cd4a249716a4d7cf87f395872091
Raj Kumar <rajkumarktr10@gmail.com>
2026-01-23 23:48:41 +0530

    Time: 16 ms (21.02%), Space: 51.6 MB (84.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,23 +1,19 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> elementCountMap = new HashMap<>();
-        
+        int len = (nums.length)/3;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            elementCountMap.put(nums[i], elementCountMap.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        List<Integer> majorityElements = new ArrayList<>();
-        int threshold = nums.length / 3;
-        
-        for (Map.Entry<Integer, Integer> entry : elementCountMap.entrySet()) {
-            int element = entry.getKey();
-            int count = entry.getValue();
-            
-            if (count > threshold) {
-                majorityElements.add(element);
+            if (map.get(nums[i]) > len){
+                set.add(nums[i]);
             }
+
         }
-        
-        return majorityElements;
+        List<Integer> list = new ArrayList<>(set);
+
+        return list;
     }
 }
