1aef7702d0e279692d0baa562d61e9d9bb2b3964
Raj Kumar <rajkumarktr10@gmail.com>
2026-02-24 23:22:43 +0530

    Time: 5 ms (16.86%), Space: 43.5 MB (41.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,28 @@
 class Solution {
     public boolean isValid(String str) {
+        Map<Character, Character> parenthesesMapping = new HashMap<>();
+            parenthesesMapping.put('(', ')');
+            parenthesesMapping.put('{', '}');
+            parenthesesMapping.put('[', ']');
 
-    Map<Character, Character> parenthesesMapping = new HashMap<>();
-    parenthesesMapping.put('(', ')');
-    parenthesesMapping.put('{', '}');
-    parenthesesMapping.put('[', ']');
-
-    Stack<Character> parentheses = new Stack<>();
-    for(int i = 0; i < str.length(); i++) {
-        char c = str.charAt(i);
-        if(parenthesesMapping.containsKey(c)) {
-            parentheses.push(c);    
-        } else {
-            if(parentheses.isEmpty()) {
+            Stack<Character> parentheses = new Stack<>();
+            for(int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if(parenthesesMapping.containsKey(c)) {
+                    parentheses.push(c);    
+                } else {
+                    if(parentheses.isEmpty()) {
+                        return false;
+                    }
+                    char target = parentheses.pop();
+                    if(!parenthesesMapping.containsKey(target) || parenthesesMapping.get(target) != c) {
+                        return false;
+                    }
+                }
+            }
+            if(!parentheses.isEmpty()) {
                 return false;
             }
-            char target = parentheses.pop();
-            if(!parenthesesMapping.containsKey(target) || parenthesesMapping.get(target) != c) {
-                return false;
-            }
-        }
+        return true;
     }
-    if(!parentheses.isEmpty()) {
-        return false;
-    }
-    return true;
-    
-}
 }
