3e164535df2b0f6a07d0f71276bebe26d101aa53
Raj Kumar <rajkumarktr10@gmail.com>
2026-03-01 23:35:20 +0530

    Time: 2 ms (99.74%), Space: 47.5 MB (16.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,25 +1,39 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        final int m = word1.length();
-        final int n = word2.length();
-        // dp[i][j] := the minimum number of operations to convert word1[0..i) to
-        // word2[0..j)
-        int[][] dp = new int[m + 1][n + 1];
+    public int findDist(String word1,String word2,int i,int j,int[][] dp)
+    {
+        if(i<0 ) return j+1;
+        if(j<0) return i+1;
+         if(dp[i][j]!=-1)return dp[i][j];
 
-        for (int i = 1; i <= m; ++i)
-            dp[i][0] = i;
+        char c1=word1.charAt(i);
+        char c2=word2.charAt(j);
+        int ins,rep,del;
+        if(c1==c2)
+          {
+            return dp[i][j] = findDist(word1,word2,i-1,j-1,dp);
+          }
+          else{
+              ins=1+findDist(word1,word2,i,j-1,dp) ;
+               rep=1+findDist(word1,word2,i-1,j-1,dp);
+               del=1+findDist(word1,word2,i-1,j,dp);
+           
+          }
+          return dp[i][j] =  Math.min(ins,Math.min(rep,del));
 
-        for (int j = 1; j <= n; ++j)
-            dp[0][j] = j;
-
-        for (int i = 1; i <= m; ++i)
-            for (int j = 1; j <= n; ++j)
-                if (word1.charAt(i - 1) == word2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
-                else
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
-
-        return dp[m][n];
-        
     }
+    public int minDistance(String word1, String word2) {
+        int l1=word1.length();
+        int l2=word2.length();
+
+        int[][] dp =new int[l1][l2];
+        for(int[] a:dp)
+        {
+            Arrays.fill(a,-1);
+        }
+         return  findDist(word1,word2,l1-1,l2-1,dp);
+    }
+        
+
+        
+    
 }
